7a540ce5b7b68a68f69431d8d2e92396fd522889
JesusSantiago31 <jesussantiagocr31@gmail.com>
2026-05-03 10:50:29 -0600

    KLJ

--- Backend/Rutas/main.py
+++ Backend/Rutas/main.py
@@ -3,7 +3,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from slowapi import _rate_limit_exceeded_handler
 from slowapi.errors import RateLimitExceeded
 from utils.limiter import limiter
-from routers import create_route, list_routes, add_stop_to_route
+from routers import create_route, list_routes, add_stop_to_route, manage_routes
 from database import Base, engine
 import models
 
@@ -29,7 +29,7 @@ def read_root():
         "message": "Bienvenido a RouteSense Rutas API",
         "docs": "/docs",
         "status": "online"
-    }
+      }
 
 app.include_router(create_route.router)
 app.include_router(list_routes.router)
