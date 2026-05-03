a64f368ed3272795c8b8f39f71f9ce433d114e18
JesusSantiago31 <jesussantiagocr31@gmail.com>
2026-05-03 13:58:28 -0600

    feat: initialize SQLAlchemy database connections for Conductores and Empresas modules

--- Backend/Conductores/database.py
+++ Backend/Conductores/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-SQLALCHEMY_DATABASE_URL = "postgresql://routesensedb_user:E4U163h5M8zofv4GvVz1Wv1NizIIDV8N@dpg-cuid6k3v2p9s73f6v0pg-a.oregon-postgres.render.com/routesensedb"
+SQLALCHEMY_DATABASE_URL = "postgresql://routesensedb_user:E4U163h5M8zofv4GvVz1Wv1NizIIDV8N@dpg-cuid6k3v2p9s73f6v0pg-a.oregon-postgres.render.com/routesensedb?sslmode=require"
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
